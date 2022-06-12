d438327afe8b3a4ee5efc5a2a3cd073c49f4b58a
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-06-12 14:47:16 +0300

    Setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCfPovIPFZbwgf8RrmnnLvEzbFN7e779DU",
+  authDomain: "clone-ae162.firebaseapp.com",
+  projectId: "clone-ae162",
+  storageBucket: "clone-ae162.appspot.com",
+  messagingSenderId: "827775120341",
+  appId: "1:827775120341:web:eeeb1ab02d226227c8035c",
+  measurementId: "G-1S2EL9WWNB"
+};
